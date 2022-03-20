32995f3952ff415d47f34f0bffb4f7dac39af843
NadyaKir <92364394+NadyaKir@users.noreply.github.com>
2022-03-20 22:48:37 +0600

    Update wsgi.py

--- pts/wsgi.py
+++ pts/wsgi.py
@@ -6,11 +6,17 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
-
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pts.settings')
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pts.settings.production")
 
-application = get_wsgi_application()
+if os.environ.get("DJANGO_SETTINGS_MODULE") == "pts.settings.production":
+    from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
+    application = Sentry(get_wsgi_application())
+else:
+    get_wsgi_application()
+    
